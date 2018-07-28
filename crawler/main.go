c9a4cbe211770622ac93f7182a34a811e687cdfa
zhangfangqing <zhangfangqing@finogeeks.com>
2018-07-28 13:40:33 +0800

    test

---
+++ crawler/main.go
@@ -0,0 +1,30 @@
+package main
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/encoding/simplifiedchinese"
+)
+
+func main() {
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		println("http get error.code=", http.StatusOK)
+		return
+	}
+
+	uft8Reader := transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())
+
+	all, err := ioutil.ReadAll(uft8Reader)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", all)
+
+}
